e7ba5fd0c2acc5bec1ead2650e0efa4d5eecbd3f
cormojs <cormorant2+github@gmail.com>
2018-01-31 10:45:53 +0900

    Migrate to webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: "./src/mastodon.js",
+    output: {
+      filename: "bundle.js",
+      path: __dirname + "/lib"
+    },
+  
+    resolve: {
+      modules: ["node_modules"],
+      // Add '.ts' and '.tsx' as resolvable extensions.
+      extensions: [".js", ".json"]
+    },
+  
+    module: {
+      rules: [
+
+      ]
+    },
+    node: {
+      "fs": "empty"
+    }
+  };
+  
